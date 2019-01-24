013d6ef4f248d620abac2aaa46e2ca6dd69d70fa
howger <360000521@qq.com>
2019-01-24 11:37:56 +0800

    update main.go

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ func main() {
 	blockChain.AddBlock("bbb")
 	blockChain.AddBlock("ccc")
 	blockChain.AddBlock("ddd")
+	blockChain.AddBlock("eee")
 
 	for key, block := range blockChain.Blocks {
 		fmt.Printf("挖到了第%d个,hash为:%x\n", key, block.NowHash)
